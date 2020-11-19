cecc7674d258ccd51d6adea640a389e038c8e3e5
Tevin Hamilton <tahamilton@student.fullsail.edu>
2020-11-18 23:48:35 -0600

    Project started
    Main Files created
    Table view is conected to view controller

---
+++ FetchListDemo/FetchListDemo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  FetchListDemo
+//
+//  Created by Tevin Hamilton on 11/18/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController,UITableViewDelegate, UITableViewDataSource {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+
+}
+
